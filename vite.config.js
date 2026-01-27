2721417734861419caf2877ea5031ebd6b34d432
cruisyconcierge <cruisyconcierge@gmail.com>
2026-01-27 18:03:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Configured for standard React/Vite deployment
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  }
 })
