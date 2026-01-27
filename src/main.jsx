2381611182e1a0dc615df5f3c2f651b1566fe700
cruisyconcierge <cruisyconcierge@gmail.com>
2026-01-27 18:07:56 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+/**
+ * React Entry Point
+ * This file mounts the main App component into the HTML root element
+ * and ensures the global CSS is applied.
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
